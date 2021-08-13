91d753cc3e93c991ebf94fc191169c5601c2b63c
dennis <690631890@qq.com>
2021-08-13 20:31:10 +0800

    handle not found static files

--- CloudClipboard/settings.py
+++ CloudClipboard/settings.py
@@ -119,5 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
