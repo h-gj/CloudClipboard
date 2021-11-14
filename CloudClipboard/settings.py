27e474b9839c49c55cf4ea6fc7a0b9466947aa7d
h-gj <690631890@qq.com>
2021-11-15 00:06:33 +0800

    refactor: change database backend

--- CloudClipboard/settings.py
+++ CloudClipboard/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'CloudClipboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CloudClipboard',
+        'USER': os.environ.get('rds_user'),
+        'PASSWORD': os.environ.get('rds_password'),
+        'HOST': 'rm-2ze61h4qw8l5r6w06to.mysql.rds.aliyuncs.com',
     }
 }
 
