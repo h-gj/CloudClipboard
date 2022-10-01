9663ae1054e1218779ca6060e4161a89aa0bd0a2
h-gj <690631890@qq.com>
2022-10-01 22:43:08 +0800

    feat: support generating qrcode

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from main.views import index, retrieve_or_delete_content, clear_all_content, ret
 
 urlpatterns = [
     path('', index),
-    path('contents/<str:pk>', retrieve_single_content),
+    path('contents/<str:pk>', retrieve_single_content, name='content'),
     path('<str:pk>/contents/clear', clear_all_content),
     path('<str:pk>', retrieve_or_delete_content, name='retrieve_or_delete_content'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render, redirect
+from urllib.parse import urlencode
+
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, render_to_response
 
 # Create your views here.
 from django.urls import reverse
@@ -11,15 +13,28 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
     else:
+        should_redirect = request.POST.get('should_redirect', '1')
         content = request.POST.get('content')
         channel_id = request.POST.get('channel_id')
         remote_addr = request.META.get('REMOTE_ADDR')
         channel, created = Channel.objects.get_or_create(id=channel_id)
-        ClipBoardContent.objects.create(
+        content_obj = ClipBoardContent.objects.create(
             content=content,
             channel=channel,
             publish_ip=remote_addr,
         )
+        path = reverse('content', kwargs={'pk': content_obj.id})
+        full_url = request.build_absolute_uri(path)
+        #  不需要重定向到channel页面，即点击分享按钮触发的请求，还是重定向到首页
+        if should_redirect == '0':
+            context = {
+                'channel_id': channel_id,
+                'content': content,
+                'content_url': full_url,
+            }
+            encoded_qs = urlencode(context)
+            return HttpResponseRedirect(f'/?{encoded_qs}')
+
         return redirect(to=reverse('retrieve_or_delete_content', kwargs={'pk': channel_id}))
 
 
